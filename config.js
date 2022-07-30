bbb3e106506ebc0cf4ec842eccc81e2e05b7c0ee
Desc <63260058+descrysama@users.noreply.github.com>
2022-07-30 22:02:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://rootuser:rootuser@cluster0.kblyp.mongodb.net/airbnb', {
+mongoose.connect('', {
     useNewUrlParser: true
 })
 .then(() => console.log('Connecté à la base de donnée'))
