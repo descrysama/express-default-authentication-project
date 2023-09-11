2dd8906fdbb580a463a7466ad10bd14a518f9276
Desc <63260058+descrysama@users.noreply.github.com>
2023-09-11 22:59:53 +0200

    updat

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const express = require('express');
-require('./config.js')
 const cookieParser = require('cookie-parser')
 const app = express();
 const port = 3000;
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+const fs = require('fs');
 app.use(cookieParser())
-// ROUTES IMPORT
 
-const userRoutes = require('./routes/userRoutes');
+app.post('/save', (req, res) => {
+    // Check if the request contains a body
+    if (req.body) {
+        // Save the req.body data to a file named "test.txt" in the root folder
+        fs.writeFileSync('test.txt', JSON.stringify(req.body));
 
-// ROUTES
-
-app.use(userRoutes);
-
+        console.log('Data saved successfully.');
+        return res.status(200).send('Data saved successfully.');
+    } else {
+        return res.status(400).send('Bad Request');
+    }
+});
 
 
 app.listen(port, () => console.log('Serveur ouvert sur le port : ' + port))
