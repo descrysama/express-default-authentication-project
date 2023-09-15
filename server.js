fbc19fd5a7ede2c5af767e421981fbdd3a4c7b16
descrysama <erwing2002@gmail.com>
2023-09-15 13:54:50 +0200

    update

--- server.js
+++ server.js
@@ -7,17 +7,8 @@ app.use(express.json());
 const fs = require('fs');
 app.use(cookieParser())
 
-app.post('/save', (req, res) => {
-    // Check if the request contains a body
-    if (req.body) {
-        // Save the req.body data to a file named "test.txt" in the root folder
-        fs.writeFileSync('test.txt', JSON.stringify(req.body));
-
-        console.log('Data saved successfully.');
-        return res.status(200).send('Data saved successfully.');
-    } else {
-        return res.status(400).send('Bad Request');
-    }
+app.get('/api', (req, res) => {
+    res.json({ message: "Hello from backend!" })
 });
 
 
